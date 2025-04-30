261b6445b94569eb36eae687b047c467e23ae81a
Mar272 <125938419+Mar272@users.noreply.github.com>
2025-04-29 23:57:28 -0500

    Ejemplo3

--- Program.cs
+++ Program.cs
@@ -1,34 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EjemploGenerics
-{
-
-    public class Caja<T>
+{ 
+    public class Repositorio<T>
     {
-        public T Contenido { get; set; }
+        private List<T> elementos = new List<T>();
 
-        public void MostrarContenido()
+        public void Agregar(T item)
         {
-            Console.WriteLine($"La caja contiene: {Contenido}");
+            elementos.Add(item);
+        }
+
+        public void MostrarTodos()
+        {
+            foreach (T item in elementos)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public int Contar()
+        {
+            return elementos.Count;
+        }
+    }
+
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+
+        public override string ToString()
+        {
+            return $"Producto: {Nombre}, Precio: {Precio:C}";
+        }
+    }
+
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public override string ToString()
+        {
+            return $"Cliente: {Nombre}, Edad: {Edad}";
         }
     }
 
     class Program
-     {
-         static void Main()
-         {
-             Caja<string> cajaDeTexto = new Caja<string>();
-             cajaDeTexto.Contenido = "Mensaje de texto";
-             cajaDeTexto.MostrarContenido();
+    {
+        static void Main()
+        {
 
-             Caja<int> cajaDeNumero = new Caja<int>();
-             cajaDeNumero.Contenido = 123;
-             cajaDeNumero.MostrarContenido();
-         }
-     } 
+            var repoProductos = new Repositorio<Producto>();
+            repoProductos.Agregar(new Producto { Nombre = "Manzana", Precio = 1.5 });
+            repoProductos.Agregar(new Producto { Nombre = "Pan", Precio = 2.0 });
 
+            Console.WriteLine("Productos:");
+            repoProductos.MostrarTodos();
+
+            var repoClientes = new Repositorio<Cliente>();
+            repoClientes.Agregar(new Cliente { Nombre = "Ana", Edad = 30 });
+            repoClientes.Agregar(new Cliente { Nombre = "Luis", Edad = 45 });
+
+            Console.WriteLine("\nClientes:");
+            repoClientes.MostrarTodos();
+
+            Console.WriteLine($"Clientes en el repositorio: {repoClientes.Contar()}");
+            Console.WriteLine($"Productos en el repositorio: {repoProductos.Contar()}");
+
+
+            Console.ReadKey();
+        }
+    }
 }
+
