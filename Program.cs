0ac1ebf76a416791671cfef51b6a71dce76ef969
Mar272 <125938419+Mar272@users.noreply.github.com>
2025-04-29 23:56:09 -0500

    Ejemplo 1 Generics

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploGenerics
+{
+
+    public class Caja<T>
+    {
+        public T Contenido { get; set; }
+
+        public void MostrarContenido()
+        {
+            Console.WriteLine($"La caja contiene: {Contenido}");
+        }
+    }
+
+    class Program
+     {
+         static void Main()
+         {
+             Caja<string> cajaDeTexto = new Caja<string>();
+             cajaDeTexto.Contenido = "Mensaje de texto";
+             cajaDeTexto.MostrarContenido();
+
+             Caja<int> cajaDeNumero = new Caja<int>();
+             cajaDeNumero.Contenido = 123;
+             cajaDeNumero.MostrarContenido();
+         }
+     } 
+
+}
